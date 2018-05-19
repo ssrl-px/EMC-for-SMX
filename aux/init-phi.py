f5ef8e966c6248b040fef9f867c3f26c14f9c4a4
tl578 <tl578@cornell.edu>
2018-05-18 21:59:58 -0400

    Add make-sym-op.py & init-phi.py

---
+++ aux/init-phi.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+fp = open("reduced-peakfiles.dat", "r")
+infiles = fp.readlines()
+fp.close()
+
+fp = open("../config.ini", "r")
+for line in fp:
+    if ("max_patch_sz" in line):
+        max_patch_sz = np.int(line.split("=")[1].strip())
+fp.close()
+
+num_data = len(infiles)
+phi = np.zeros(num_data)
+phi_sum = 0.
+for d in xrange(num_data):
+    infile = infiles[d].strip()
+    fp = open(infile, "r")
+    content = fp.readlines()
+    fp.close()
+
+    num_peak = len(content)/2
+    num_diffuse_peak = 0
+    for i in xrange(num_peak):
+        patch_sz = int(content[2*i].strip())
+        if (patch_sz > max_patch_sz):
+            num_diffuse_peak += 1
+            continue
+        words = content[2*i+1].split()
+        for j in xrange(patch_sz):
+            phi[d] += np.double(words[2*j+1])
+    phi[d] /= (num_peak - num_diffuse_peak)
+    phi_sum += phi[d]
+
+fout = open("start-phi.dat", "w")
+rescale = num_data/phi_sum
+for d in xrange(num_data):
+    line = "%1.15e\n" % (phi[d]*rescale)
+    fout.write(line)
+fout.close()
